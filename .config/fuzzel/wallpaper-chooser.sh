2bc9572204ed15e28274dc4d16c4ef3b609112e1
tau-lavender <thesigmabase@gmail.com>
2026-02-26 18:13:53 +0300

    wallpaper-chooser fix

--- .config/fuzzel/wallpaper-chooser.sh
+++ .config/fuzzel/wallpaper-chooser.sh
@@ -1,3 +1,4 @@
 wall_dir="${HOME}/pictures/wallpapers/current/"
-wall_selection="find "${HOME}/pictures/wallpapers/current/" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec basename {} \; | fuzzel -d"
+wall_selection=$(find "${wall_dir}" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec basename {} \; | fuzzel --dmenu)
+# \0icon\x1f
 swaymsg "output * background ${wall_dir}/${wall_selection} fill"
