c49bf12041ec26ce5bd3da9871a6fa9b74e08165
tau-lavender <thesigmabase@gmail.com>
2026-03-24 14:08:15 +0300

    feat: add nvim telescope bind for find functions

--- stow/.config/nvim/lua/lavender/plugins/telescope.lua
+++ stow/.config/nvim/lua/lavender/plugins/telescope.lua
@@ -13,6 +13,7 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>fd', function() builtin.lsp_document_symbols({ symbols='function' }) end, { desc = 'Telescope find files' })
 
         telescope.setup({
             defaults = {
